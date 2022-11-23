273ec67b8b353723bca3bc44b5aabb48373f7875
Drew <an538926@dal.ca>
2022-11-23 09:20:13 -0400

    adding basic commands

---
+++ api/checkUser.js
@@ -0,0 +1,40 @@
+// this file will contain all of the required
+// functions in order to make database calls
+// and assign user's their proper roles based on
+// what their role in the DB / Memberstack
+const { MongoClient } = require('mongodb');
+const { databaseUri } = require('../config.json');
+const { forex, crypto, stocks, futures, diamond } = require('../roles.json');
+// connection url
+const url = '';
+const client = new MongoClient(databaseUri);
+
+const dbName = 'SimplePicks'
+
+async function connectToDb() {
+  // connect to MongoDB client
+  await client.connect();
+  console.log('successfully connected to the server');
+  const db = client.db(dbName);
+
+  const collection = db.collection('Users');
+  // console.log(collection)
+  // now return a list of all users as a json object
+  const getUsers = await collection.find({}).toArray();
+  console.log('getUsers');
+  console.log(getUsers.length)
+  const diamondUsers = getUsers.filter((user) => {
+    return user.discord_role === stocks;
+  })
+  console.log('Diamond Users:')
+  console.log(diamondUsers);
+  console.log()
+
+  // return 
+  return 'done';
+
+}
+
+connectToDb().then(console.log).catch(console.error).finally(() => client.close());
+
+// module.exports = { connectToDb };
--- index.js
+++ index.js
@@ -1,12 +1,55 @@
-const { Client, Events, GatewayInitBits } = require('discord.js');
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
+const commandsPath = path.join(__dirname, 'commands');
+const commandsFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('js'));
 
 // creates new client instance
-const client = new Client({ intents: [GatewayInitBits.Guilds] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.commands = new Collection();
+
+// import commands from files 
+for (const file of commandsFiles) {
+  const filePath = path.join(commandsPath, file);
+  const command = require(filePath);
+  if ('data' in command && 'execute' in command) {
+    client.commands.set(command.data.name, command);
+  } else {
+    console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "excute" property`);
+  }
+}
+
+// set up interation listener -> 
+client.on(Events.InteractionCreate, async interaction => {
+  console.log(interaction);
+
+  if (!interaction.isChatInputCommand()) {
+    console.log('this is a slash command');
+    https://discord.com/api/oauth2/authorize?client_id=1044775867722768464permissions=8&scope=bot%20applications.commands
+
+    return;
+  }
+
+  const command = interaction.client.commands.get(interaction.commandName);
+
+  if (!command) {
+    console.error(`No command matching ${interaction.commandName} was found`);
+
+    try {
+      await command.execute(interaction);
+    } catch (error) {
+      console.error(error);
+      await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true })
+    }
+  }
+})
 
 // only runs once client is ready
 client.once(Events.ClientReady, c => {
   console.log(`Ready! Logged in as ${c.user.tag}`);
+  console.log(c.guilds);
 });
 
 client.login(token);
